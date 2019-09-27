4dda790c8fdb142291730c774adbc2ed78b3e721
FizeChan <chenfengzhan@qq.com>
2019-09-27 11:45:01 +0800

    独立框架项目文件到单独git项目，方便框架代码提交。

--- src/Session.php
+++ src/Session.php
@@ -13,45 +13,4 @@ class Session extends FizeSession
     {
         parent::__construct($config);
     }
-
-    /**
-     * 获取一个缓存
-     * @param string $name 缓存名
-     * @param mixed $default 默认值
-     * @return mixed
-     */
-    public static function get($name, $default = null)
-    {
-        return self::$cache->get($name, $default);
-    }
-
-    /**
-     * 查看指定缓存是否存在
-     * @param string $name 缓存名
-     * @return bool
-     */
-    public static function has($name)
-    {
-        return self::$cache->has($name);
-    }
-
-    /**
-     * 设置一个缓存
-     * @param string $name 缓存名
-     * @param mixed $value 缓存值
-     * @param int $expire 有效时间，以秒为单位,0表示永久有效
-     */
-    public static function set($name, $value, $expire = null)
-    {
-        self::$cache->set($name, $value, $expire);
-    }
-
-    /**
-     * 删除一个缓存
-     * @param string $name 缓存名
-     */
-    public static function remove($name)
-    {
-        self::$cache->remove($name);
-    }
 }
